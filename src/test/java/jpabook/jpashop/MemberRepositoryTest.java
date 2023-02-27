2f6d258f92850d43665fe7e585bbfe83afdcea51
dahyunhan <dollae7020@gmail.com>
2023-02-27 18:24:42 +0900

    fix

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -3,7 +3,6 @@ package jpabook.jpashop;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -17,7 +16,7 @@ class MemberRepositoryTest {
 
     @Test
     @Transactional
-    @Rollback(value = false)
+//    @Rollback(value = false)
     void testMember() {
 
         Member member = new Member();
