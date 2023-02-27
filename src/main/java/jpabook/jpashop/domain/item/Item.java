092a2da1974f5fc123b7687448518a6327302993
dahyunhan <dollae7020@gmail.com>
2023-02-28 03:36:07 +0900

    create entity2

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) //테이블 전략
@@ -19,5 +21,9 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
 
 }
--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,36 +1,13 @@
-//package jpabook.jpashop;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import static org.assertj.core.api.Assertions.*;
-//
-//
-//@SpringBootTest
-//class MemberRepositoryTest {
-//
-//    @Autowired
-//    MemberRepository memberRepository;
-//
-//    @Test
-//    @Transactional
-////    @Rollback(value = false)
-//    void testMember() {
-//
-//        Member member = new Member();
-//        member.setUsername("userA");
-//
-//        Long saveId = memberRepository.save(member);
-//        Member findMember = memberRepository.find(saveId);
-//
-//        assertThat(findMember.getId()).isEqualTo(member.getId());
-//        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-//
-//        System.out.println("MemberId = " + findMember.getId());
-//        System.out.println("userName = " + findMember.getUsername());
-//    }
-//
-//
-//}
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
